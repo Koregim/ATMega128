301b6fbf321adb4f9edf555ebf2612c45ad78b91
Koregim <fpdl4848@gmail.com>
2024-10-02 16:52:44 +0900

    3

--- test01/test01/main.c
+++ test01/test01/main.c
@@ -1,19 +1,19 @@
-/*
- * test01.c
- *
- * Created: 2024-10-02 오후 12:17:15
- * Author : user
- */ 
-
-#include <avr/io.h>
+#include <avr/io.h>				//avr 기본 헤더
+#include <util/delay.h>
+#define F_CPU 10000000L
+//LED Port : PG4
 
 
 int main(void)
 {
     /* Replace with your application code */
-	printf("Hello, world");
+	DDRG |= 0x10;			// = xxx1 0000 : 1 = 출력 0 = 입력, |= xxx1 xxxx
     while (1) 
     {
+		PORTG |= 0x01;
+		_delay_ms(500);
+		PORTG = 0x00;
+		_delay_ms(500);
     }
 }
 
